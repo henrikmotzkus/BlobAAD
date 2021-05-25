6e7e65381f2c8a1253f49665b88100583bc6f6d5
Henrik Motzkus <hemotzku@microsoft.com>
2021-05-25 16:57:47 +0200

    key handling

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -13,10 +13,15 @@ if (-not $file) {
 }
 
 if ($file) {
-    $key = "ijcLjf4Zm33zt2RDTEBF63ov5StCQLEVq7HMU2AmfqCpK2j+80+Lws1ADS3ABoIBElhrB4DQAxxEdZADmtFQYA=="
-    $saname = "bdfblobaad"
-    $containername = "protected"
-    $body = "No parameter provided"
+
+    $saname = Get-ChildItem env:APPSETTING_saname
+    $containername = Get-ChildItem env:APPSETTING_containername
+    $key = Get-ChildItem env:APPSETTING_sakey
+    
+    #$key = "ijcLjf4Zm33zt2RDTEBF63ov5StCQLEVq7HMU2AmfqCpK2j+80+Lws1ADS3ABoIBElhrB4DQAxxEdZADmtFQYA=="
+        #$saname = "bdfblobaad"
+    #$containername = "protected"
+    #$body = "No parameter provided"
     
     $ctx = New-AzStorageContext -StorageAccountName $saname -StorageAccountKey $key
     
