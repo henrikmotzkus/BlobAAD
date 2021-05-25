89cebbafe3b2e80d4a9abc0b0eafc3a599055aa6
Henrik Motzkus <hemotzku@microsoft.com>
2021-05-25 17:39:14 +0200

    debug

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -17,14 +17,11 @@ if ($file) {
     $saname = Get-ChildItem env:APPSETTING_saname
     $containername = Get-ChildItem env:APPSETTING_containername
     $key = Get-ChildItem env:APPSETTING_sakey
-    
-    #$key = "ijcLjf4Zm33zt2RDTEBF63ov5StCQLEVq7HMU2AmfqCpK2j+80+Lws1ADS3ABoIBElhrB4DQAxxEdZADmtFQYA=="
-        #$saname = "bdfblobaad"
-    #$containername = "protected"
-    #$body = "No parameter provided"
-    
+    Write-Host $key    
     $ctx = New-AzStorageContext -StorageAccountName $saname -StorageAccountKey $key
     
+
+
     $StartTime = Get-Date
     $EndTime = $startTime.AddMinutes(2.0)
     
