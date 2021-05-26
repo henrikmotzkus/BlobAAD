f7538ab43aa2da34eae3464d98114f6106de1e40
Henrik Motzkus <hemotzku@microsoft.com>
2021-05-26 21:51:30 +0200

    error handling

--- HttpTrigger1/run.ps1
+++ HttpTrigger1/run.ps1
@@ -4,7 +4,7 @@ using namespace System.Net
 param($Request, $TriggerMetadata)
 
 # Write to the Azure Functions log stream.
-Write-Host "PowerShell HTTP trigger function processed a request."
+# Write-Host "PowerShell HTTP trigger function processed a request."
 
 # Interact with query parameters or the body of the request.
 $file = $Request.Query.file
@@ -13,26 +13,32 @@ if (-not $file) {
 }
 
 try{
-    
     if ($file) {
-    
+        
+        # Getting settings
         $saname = $env:APPSETTING_saname
         $containername = $env:APPSETTING_containername
         $key = $env:APPSETTING_sakey
+
+        #Generating a context for using in subsequent calls
         $ctx = New-AzStorageContext -StorageAccountName $saname -StorageAccountKey $key
+
+        # Check if blob exists
+        $blob = Get-AzureStorageBlob -Blob $file -Container $containername -Context $ctx -ErrorAction Stop
+
+        # Creating the SAS token
         $StartTime = Get-Date
         $EndTime = $startTime.AddMinutes(2.0)
-        
         $location = New-AzStorageBlobSASToken -Container $containername -Blob $file -Permission r -StartTime $StartTime -ExpiryTime $EndTime -FullUri -Context $ctx
         
     }
 }
 Catch {
     # Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::BadRequest
-    Body = "Nothing found"
-})
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode = [HttpStatusCode]::BadRequest
+        Body = "Nothing found"
+    })
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
